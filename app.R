3152a662e6cc9c1735dabc644b431cb48bcf8cc9
Daniel Lapido Martínez <100417214@alumnos.uc3m.es>
2020-03-05 19:06:25 +0100

    Added first map

--- app.R
+++ app.R
@@ -7,6 +7,7 @@ library(plotly)
 library(dplyr)
 library(ggplot2)
 library(ggthemes)
+library(leaflet)
 library(extrafont)
 
 Confirmed <- readr::read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
@@ -19,8 +20,8 @@ Recovered <- readr::read_csv("https://raw.githubusercontent.com/CSSEGISandData/C
 
 report <- cbind(Confirmed[,c(1,2,3,4,ncol(Confirmed))], Deaths[,ncol(Deaths)], Recovered[,ncol(Recovered)] )
 
-names(report)[c(5,6,7)] <-c("Confimed", "Deaths", "Recovered")
-
+#names(report)[c(3,4,5,6,7)] <-c("lat","long","Confirmed", "Deaths", "Recovered")
+names(report)[c(5,6,7)] <-c("Confirmed", "Deaths", "Recovered")
 
 
 conf2 <- reshape2::melt(Confirmed, id.vars = colnames(Confirmed)[1:4])
@@ -30,6 +31,12 @@ deaths2 <- reshape2::melt(Deaths, id.vars = colnames(Deaths)[1:4])
 recovered2 <-  reshape2::melt(Recovered, id.vars = colnames(Recovered)[1:4])
 
 
+mapPanel <- tabPanel("map",
+                     leafletOutput('map', width = '100%', height = '300px'),
+                     DT::DTOutput("esTable")
+)
+
+
 dataPanel <- tabPanel("Data", 
                       tableOutput("dataTable")
 )
@@ -37,6 +44,7 @@ dataPanel <- tabPanel("Data",
 
 ui <- navbarPage("Shiny app",
                  dataPanel,
+                 mapPanel,
                  inverse=TRUE
                  
 )
@@ -52,6 +60,17 @@ server <- function(input, output){
   output$dataTable <- renderTable({
     head(report)
   })
+  
+  output$map <- renderLeaflet({
+    
+    report %>%
+    leaflet() %>%
+    addTiles() %>%
+    addCircles()
+
+  })
+  
+  output$esTable <- DT::renderDT(head(report))
  
   
 }
